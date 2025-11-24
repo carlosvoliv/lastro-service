8a7064945209ed303a18cc676d0c79af0050bbc4
carlosvoliv <carlos.oliveira@tecksolucoes.com.br>
2025-11-24 12:04:17 -0300

    lastro Item

--- app/Models/LastroItem.php
+++ app/Models/LastroItem.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class LastroItem extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    protected $guarded = ['id'];
+
+    // Relacionamento inverso: O Item pertence a um Lote
+    public function batch()
+    {
+        return $this->belongsTo(LastroBatch::class, 'batch_id');
+    }
 }
