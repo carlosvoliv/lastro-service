9dacc49817eca61ff883687e5ba87e81e578152c
carlosvoliv <carlos.oliveira@tecksolucoes.com.br>
2025-11-24 12:02:34 -0300

    Lastro Batch

--- app/Models/LastroBatch.php
+++ app/Models/LastroBatch.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids; // Importante para UUID
 
 class LastroBatch extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    // Protege apenas o ID, o resto pode ser preenchido
+    protected $guarded = ['id'];
+
+    // Relacionamento: Um Lote tem muitos Itens
+    public function items()
+    {
+        return $this->hasMany(LastroItem::class, 'batch_id');
+    }
 }
